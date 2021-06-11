9282c290c4951f6b8d7304a7f68940f0718c06b1
David Daniels <David@DavidDaniels.com>
2021-06-11 16:30:37 -0400

    added search and 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+
+<div class="container-page">
+<h1>Are you lost???</h1>
+
+
+</div>
+
+
+<?php get_footer(); ?>
---
+++ searchpage.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Search Page
+*/
+?>
+
+<?php get_header(); ?>
+
+    <div class="container-page">
+        <?php get_search_form(); ?>
+    </div>
+
+<?php get_footer(); ?>
